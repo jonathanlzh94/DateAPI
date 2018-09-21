878f83ccfc9019980f77cf9a0481eac01a5031da
mephet <matthew_kohjm@hotmail.sg>
2018-09-21 17:49:03 +0800

    Create Main.java

---
+++ S2Q2/Main.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner sc = new Scanner(System.in);
+		String date1 = sc.nextLine();
+		String date2 = sc.nextLine();
+		UserMainCode.displayAge(date1, date2);
+	}
+
+}
